f7baf2477484b94e61b106772ed2e4fc96e862e3
Som <somaiah1988@gmail.com>
2023-03-08 09:46:04 -0500

    11

---
+++ PracPrograms/src/Reverse_String.java
@@ -0,0 +1,23 @@
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+		// INPUT : WELCOME
+		//OUTPUT: EMOCLEW
+		
+		String input="SELENIUM TRAINING";
+		int len=input.length();
+		//System.out.println(len);
+		
+		for(int i=len-1;i>=0;i--) {
+		System.out.print(input.charAt(i));
+
+		
+		
+		}
+		
+		
+
+	}
+
+}
